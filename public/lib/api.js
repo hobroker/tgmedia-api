c0436a9e9087153916b8c74be8bd81a40f9c7311
Igor <4361921+hobroker@users.noreply.github.com>
2022-10-11 23:09:02 +0300

    add tabs to UI

--- public/lib/api.js
+++ public/lib/api.js
@@ -12,6 +12,19 @@ class API {
           .sort((a, b) => a.title.localeCompare(b.title)),
       );
   }
+  getShows() {
+    return fetch('/v1/media/shows')
+      .then((response) => response.json())
+      .then((response) =>
+        response
+          .map(({ images, ...movie }) => ({
+            ...movie,
+            poster: images.find(({ coverType }) => coverType === 'poster')
+              ?.remoteUrl,
+          }))
+          .sort((a, b) => a.title.localeCompare(b.title)),
+      );
+  }
 
   sendMovie(id) {
     return fetch(`/v1/messenger/${id}`);
--- public/lib/table.js
+++ public/lib/table.js
@@ -1,5 +1,6 @@
 class Table {
-  constructor() {
+  constructor(tabs) {
+    this.tabs = tabs;
     this.table = document.getElementById('table');
     this.tbody = document.querySelector('tbody');
     this.tableLoading = document.getElementById('table-loading');
@@ -9,6 +10,11 @@ class Table {
   show() {
     this.table.classList.remove('is-hidden');
     this.tableLoading.classList.add('is-hidden');
+  }
+
+  hide() {
+    this.table.classList.add('is-hidden');
+    this.tableLoading.classList.remove('is-hidden');
   }
 
   addActions() {
@@ -26,7 +32,8 @@ class Table {
     });
   }
 
-  async render() {
+  async renderMovies() {
+    this.hide();
     const movies = await this.api.getMovies();
 
     this.tbody.innerHTML = movies
@@ -56,8 +63,36 @@ class Table {
 
     this.show();
   }
+
+  async renderShows() {
+    this.hide();
+    const movies = await this.api.getShows();
+
+    this.tbody.innerHTML = movies
+      .map(({ title, overview, poster, id, movieFile }) => {
+        const disabled = movieFile ? '' : 'disabled';
+
+        return `<tr>
+            <td class="w-100">
+              <img src="${poster}" alt="${title}" class="image w-100" />
+            </td>
+            <td class="w-192">
+              <p class="title is-6">${title}</p>
+            </td>
+            <td>
+              <p class="subtitle is-6">${overview}</p>
+            </td>
+            <td class="w-100 has-text-centered">
+              <button class="button is-primary is-small" data-id="${id}" ${disabled}>
+                ${disabled ? 'No file' : 'Send'}
+              </button>
+            </td>
+          </tr>`;
+      })
+      .join('');
+
+    this.addActions();
+
+    this.show();
+  }
 }
-
-const $table = new Table();
-
-$table.render();
