97a8c937a4908d0c5f0fe9d32a265389aa4e12cb
Igor <4361921+hobroker@users.noreply.github.com>
2022-10-14 12:55:22 +0300

    remove encoded file when donw

--- src/modules/handbrake/services/handbrake.service.ts
+++ src/modules/handbrake/services/handbrake.service.ts
@@ -1,12 +1,13 @@
 import { spawn } from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { handbrakeConfig } from '../handbrake.config';
 
 export type HandbrakeConvertOptions = {
   input: string;
-  outputFilename: string;
+  outputFilename: string | number;
   ext?: '.mp4';
 };
 
@@ -33,6 +34,12 @@ export class HandbrakeService {
       this.config.tmpFolder,
       `${outputFilename}${ext}`,
     );
+    const outputExists = await this.fileExists(output);
+
+    if (outputExists) {
+      return output;
+    }
+
     const log = (data) => {
       const text = data.split('\n').find((item) => item.includes('Encoding'));
 
@@ -65,7 +72,43 @@ export class HandbrakeService {
     });
   }
 
+  removeFileIfNeeded({ outputFilePath }: { outputFilePath: string }) {
+    if (!this.config.removeFileWhenDone) {
+      return;
+    }
+
+    const { dir } = path.parse(outputFilePath);
+    const shouldDelete = dir.endsWith(this.config.tmpFolder);
+
+    if (!shouldDelete) {
+      return;
+    }
+
+    this.logger.error('removing file', outputFilePath);
+
+    return new Promise((resolve) => {
+      fs.rm(outputFilePath, (err) => {
+        if (err) {
+          this.logger.error('error removing file', outputFilePath, err);
+
+          return resolve(false);
+        }
+
+        this.logger.error('removed file', outputFilePath);
+        resolve(true);
+      });
+    });
+  }
+
   private shouldConvert(input: string) {
     return this.config.alwaysConvert || !input.endsWith('.mp4');
+  }
+
+  private fileExists(path: string) {
+    return new Promise((resolve) =>
+      fs.access(path, fs.constants.F_OK, (err) => {
+        resolve(!err);
+      }),
+    );
   }
 }
